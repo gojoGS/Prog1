f42ffa5f3e271f73e93af81e52cc10628446e2bc
Bartha Zoltán <barthazoli00@gmail.com>
2021-03-30 13:44:12 +0200

    nested.hs

--- etc/week08/algo/nested.hs
+++ etc/week08/algo/nested.hs
@@ -7,10 +7,19 @@ value c
 nesting :: String -> Integer
 nesting s = maximum (scanl1 (+) (map value s))
 
+-- Ugyanez egy sorban, lambdákkal
+-- nesting :: String -> Integer
+-- nesting s = maximum . scanl1 (+) $ map (\x -> if x == '(' then 1 else -1) (filter (`elem` "()") s)
+
+-- Ugyanűgy egy sorban, de pipeline-al, nem fv hívással
+-- nesting :: String -> Integer
+-- nesting s = maximum (scanl1 (+) (map (\x -> if x == '(' then 1 else -1) (filter (`elem` "()") s)))
+
+-- nesting :: String -> Integer
 -- nesting s = maximum . scanl1 (+) $ map value s
 
 tests :: [[Char]]
 tests = ["()", "(a + b)", "((a + b) * 3)", "3 + 4", "((((()))))"]
 
 main :: IO ()
-main = print (map nesting tests)
+main = print (zip tests (map nesting tests))
